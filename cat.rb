015d41e62493586fa8403c39ed6efa942329ebcb
Ryan <ryan.y.chung@gmail.com>
2016-08-11 15:26:59 -0400

    added horribly inefficient instance that returns time of day

--- cat.rb
+++ cat.rb
@@ -6,6 +6,20 @@ class Cat
     @meal_time = meal_time
   end
 
+  def eats_at
+    if @meal_time == 0
+      return "12 AM"
+    elsif @meal_time < 12
+      return "#{@meal_time} AM"
+    elsif @meal_time == 12
+      return "12 PM"
+    elsif @meal_time <= 23
+      return "#{@meal_time - 12} PM"
+    else
+      return "learn how to tell time"
+    end
+  end
+
 end
 
 buttons = Cat.new("Mr. Buttons", "mouse", 10)
