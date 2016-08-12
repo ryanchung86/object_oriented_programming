de8ce0595b0fd2714dff23926dfb6f52900e18b0
Ryan <ryan.y.chung@gmail.com>
2016-08-11 23:33:34 -0400

    trying to retrieve the inputs for rover and mars will fear my coding powers

--- rover.rb
+++ rover.rb
@@ -6,4 +6,19 @@ def initialize(x, y, direction)
   @direction = direction
 end
 
+def read_instruction
 end
+
+end
+
+pathfinder = Rover.new(1, 2, "N")
+sojourner = Rover.new(3, 3, "E")
+
+puts "How wide is the plateau?"
+width = gets.chomp
+
+puts "How long is the plateau?"
+length = gets.chomp
+
+puts "What are my instructions?"
+moves = gets.chomp
