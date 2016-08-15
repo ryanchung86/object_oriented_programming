d844811fcfe494d3622fdd89eb3764a9e0df746c
Ryan <ryan.y.chung@gmail.com>
2016-08-14 21:09:09 -0400

    got quota method to work

--- paperboy.rb
+++ paperboy.rb
@@ -1,32 +1,49 @@
 class Paperboy
 
-# attr_reader :earnings
-# attr_accessor :experience
+attr_accessor :earnings
+attr_accessor :experience
 
   def initialize(name, side)
     # create paperboy instance
     @name = name
-    @experience = 0
     @side = side
+    @experience = 0
     @earnings = 0
 
   end
 
   def quota
     # return paperboy's quota for next delivery
-    50 + @experience / 2
+    puts 50 + @experience / 2
   end
 
   def deliver(start_address, end_address)
     # start and end address is given,
     # amount of money earned and updated experience is returned
     # get number of houses based on @side
-    
+    (start_address..end_address).each do |house|
+    #   if (@side == "even") && (house % 2 == 0)
+    #
+    #   end
+      @experience += 1
+    end
+
+  end
+
+  def income
+
   end
 
   def report
-    # return string about paperboy's performance, including
-    # updated experience
+    # return string about paperboy's performance
+    "I'm #{@name}, I've delivered #{quota} papers and I've earned"
   end
 
 end
+
+tommy = Paperboy.new("Tommy", "even")
+
+tommy.quota
+tommy.deliver(101, 220)
+tommy.earnings
+tommy.report
